cb59282513659519d8fa1ef12721a45f07c94df8
imramu00 <imramu00@gmail.com>
2021-05-19 20:16:40 +0530

    Sort change

--- sort2.rb
+++ sort2.rb
@@ -1,2 +1,2 @@
 array = [{:width=>3,:height=>2},{:width=>1,:height=>1},{:width=>1,:height=>2}]
-p array.sort { |i,j| (j[:width]*j[:height]) <=> (i[:width]*i[:height]) }
+p array.sort_by{|j| j[:width]*j[:height]}.reverse!
